3e7edf67f24ba33651734bda1017a0698341d233
codeftshaunak <sagardeyyash19@gmail.com>
2024-03-08 15:24:10 +0600

    update the features spam

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -50,17 +50,39 @@ const SpamDetectionForm: React.FC = () => {
 
     const convertToPastTense = () => {
         let doc = nlp(message);
-        doc.verbs().toPastTense();
+        doc.sentences().toPastTense();
         if (message.length > 0) {
             toast(doc.text());
-            setMessage("")
+            setMessage(doc.text())
         } else {
             toast("You can't make empty text to past!!!");
         }
     };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const convertToFutureTense = () => {
+        let doc = nlp(message);
+        doc.sentences().toFutureTense();
+        if (message.length > 0) {
+            toast(doc.text());
+            setMessage(doc.text())
+        } else {
+            toast("You can't make empty text to future!!!");
+        }
+    };
+
+    const convertToPresentTense = () => {
+        let doc = nlp(message);
+        doc.sentences().toPresentTense();
+        if (message.length > 0) {
+            toast(doc.text());
+            setMessage(doc.text())
+        } else {
+            toast("You can't make empty text to present!!!");
+        }
+    };
+
+
+    const handleSubmit = () => {
         // Check if past tense message contains any of the spam words
         const spamWords = ['investment', 'spam', 'money', 'hero', 'fuck'];
         const containsSpamWord = spamWords.some(word => message.toLowerCase().includes(word));
@@ -71,19 +93,25 @@ const SpamDetectionForm: React.FC = () => {
         if (isSpam) {
             // Show toast notification directly without using promises
             toast("This message appears to be spam!");
+        } if (!isSpam) {
+            toast(message);
+            setMessage("")
         }
     };
 
     return (
         <div className='w-full flex flex-col py-10'>
-            {isSpam && <p className="text-center mb-5">This message appears to be spam.</p>}
-            <form onSubmit={handleSubmit} className='flex flex-col w-[400px] m-auto bg-slate-400 p-6'>
-                <textarea value={message} onChange={handleInputChange} className='text-black p-3' />
-                <button className="text-black py-2 mt-4 bg-white w-36 m-auto rounded-md" onClick={() => convertToPastTense()}>Convert to past</button>
-                <button className="text-black py-2 mt-4 bg-white w-36 m-auto rounded-md" onClick={() => convertToPastTense()}>Convert to future</button>
-                <button className="text-black py-2 mt-4 bg-white w-36 m-auto rounded-md" onClick={() => convertToPastTense()}>Convert to present</button>
-                <button type="submit" className="text-black py-2 mt-4 bg-white w-36 m-auto rounded-md">Submit</button>
-            </form>
+            {isSpam && <p className="text-center mb-5 text-red-400">This message appears to be spam.</p>}
+            <p className="text-center mb-5 text-black"></p>
+            <div className='flex flex-col w-[500px] m-auto bg-slate-400 p-6 rounded-lg'>
+                <textarea value={message} onChange={handleInputChange} className='text-black p-3 rounded-lg' />
+                <div className="flex">
+                    <button className="text-black py-2 mt-4 bg-white w-36 m-auto rounded-md" onClick={() => convertToPastTense()}>Convert to past</button>
+                    <button className="text-black py-2 mt-4 bg-white w-36 m-auto rounded-md" onClick={() => convertToFutureTense()}>Convert to future</button>
+                    <button className="text-black py-2 mt-4 bg-white w-36 m-auto rounded-md" onClick={() => convertToPresentTense()}>Convert to present</button>
+                </div>
+                <button onClick={() => handleSubmit()} className="text-black py-2 mt-4 bg-white w-36 m-auto rounded-md">Submit</button>
+            </div>
         </div>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function Home() {
   return (
     <>
+      <h1 className="text-center mt-36 text-2xl">Spam detector & sentence convertor</h1>
       <ToastContainer />
       <SpamDetectionForm />
     </>
